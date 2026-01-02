62301249e847b3cbb7dad0d9ed0e258f464ea525
cameron <cam.k801@gmail.com>
2026-01-02 14:47:08 -0500

    added license to files

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
+/*
+
+    nesemu, a Nintendo Entertainment System emulator
+    Copyright (C) 2026  Cameron Kelly
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+*/
+
+
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_render.h>
@@ -132,7 +155,7 @@ int main(int argc, char* argv[]){
   uint8_t* fileBuffer;
 
   FILE* fptr;
-
+  printf("    nesemu  Copyright (C) 2026  Cameron Kelly \n This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'. \n This is free software, and you are welcome to redistribute it \n under certain conditions; type `show c' for details.");
 
   // parsing command line arguments
   if(argc > 1){
--- memorybus.c
+++
@@ -1,2 +0,0 @@
-#include "memorybus.h"
-
